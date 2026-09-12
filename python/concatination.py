264c9c94dd033a90c614aa5724b1aeb743fd59e2
sania2331 <saniaahmed9000@gmail.com>
2026-09-12 11:20:23 -0700

    new files

---
+++ python/concatination.py
@@ -0,0 +1,9 @@
+greeting = "hello"
+addresses = "world"
+my_world = greeting + addresses
+print(my_world)
+
+name = "sania"
+lastname = "Ahmed"
+full_name = name+lastname
+print(full_name)
---
+++ python/legal and ilegall.py
@@ -0,0 +1,28 @@
+# legal variable and illegal variable name.
+
+# legal
+num = 2
+num +=4
+print(num)
+# ilegal 
+num = 3
+num ++5
+#   print(num)    syntax invaild error
+# A. Normal Letters & Underscores
+user_name = "Sania"
+age = 22
+
+# B. Numbers at the end or middle (Not at the start)
+user1 = "Alice"
+score_2_beat = 100
+
+# C. Starting with an Underscore
+_private_data = "Secret"
+__count = 5
+
+# D. CamelCase & Uppercase
+totalAmount = 2500
+MAX_LIMIT = 50
+
+# Output check
+print(user_name, age, user1, score_2_beat, totalAmount)
