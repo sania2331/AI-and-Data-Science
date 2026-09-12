d50fc868b55b1a2df61d4919a0dda033ab1b8098
sania2331 <saniaahmed9000@gmail.com>
2026-09-12 10:00:20 -0700

    new update

---
+++ python/Operators.py
@@ -0,0 +1,15 @@
+num = 23
+total = num + 200
+print(total)
+# subtract
+num = 46
+total = num - 34
+print(total)
+# Multiply
+num = 70
+total = num * 34
+print(total)
+# Divide
+num = 23
+total = num /30
+print(total)
---
+++ python/variable.py
@@ -0,0 +1,7 @@
+name = "marks"
+age = 20
+subject = "Maths"
+print(name)
+print(subject)
+your_age = 31
+print(your_age)
