1d4821ee6aa9e11c6531e4ee25ba23d0c006cb57
sania2331 <saniaahmed9000@gmail.com>
2026-09-12 11:33:18 -0700

    new file added

---
+++ python/statements.py
@@ -0,0 +1,12 @@
+
+students = "present"
+
+if students == "present":
+    print("yes all student present")
+    if 2 + 2 == 4:
+        print("everything is perfect")
+        # if and else condition
+        if species == "cat":
+print("yes its cat")
+else:
+(its not cat.)
