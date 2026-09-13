302790503092f0625957cb7974abc8eee01650e5
sania2331 <saniaahmed9000@gmail.com>
2026-09-13 02:13:10 -0700

    new files added

---
+++ python/else,elif.py
@@ -0,0 +1,18 @@
+# age = 18
+# if age < 12:
+#     print("child")
+# elif age >=18:
+#     print("Adult")
+# elif age >=13 :
+#     print("Teenager")
+# else:
+#     print("unknown")
+
+
+score = 95
+if score > 90:
+        print("Excellent")
+elif score < 30:
+        print("fail")
+else:
+   print("fair")
---
+++ python/list.py
@@ -0,0 +1,13 @@
+fruit = ["apple" , "Banana" , "cherry" , "star"]
+print("phela fruit" ,fruit [0])
+print("last fruit" ,fruit [3])
+#for adding in list used Appened
+fruit.append("mango")
+print(fruit)
+# and insert used for indexing in list adding
+fruit.insert(   5,"peach")
+print(fruit)
+fruit.remove("cherry")
+print(fruit)
+fruit.pop()
+print(fruit)
